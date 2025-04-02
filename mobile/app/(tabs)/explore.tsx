474364996111f517e03342d3d7a78ff243b48469
FantasmeiroFantasma <163547571+GustaioRocodaio@users.noreply.github.com>
2025-04-02 12:19:58 -0300

    Update explore.tsx

--- mobile/app/(tabs)/explore.tsx
+++ mobile/app/(tabs)/explore.tsx
@@ -21,18 +21,25 @@ export default function TabTwoScreen() {
         <ThemedText type="title">Aqui você encontra respostas para duvidas sobre o próximo Silent Hill</ThemedText>
       </ThemedView>
       <ThemedText></ThemedText>
-      <Collapsible title="File-based routing">
+      <Collapsible title="Quais os requistios para rodar no meu PC?">
         <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
+          {' '}
+          <ThemedText type="defaultSemiBold">
+          Sistema Operacional: Windows 11 x64.
+            
+          Processador: Intel Core i5-8400 / AMD Ryzen 5 2600. 
+             
+          Memória RAM: 16 GB</ThemedText> {' '}
+          
+         
+          <ThemedText type="defaultSemiBold"></ThemedText>
         </ThemedText>
         <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
+           <ThemedText type="defaultSemiBold"></ThemedText>{' '}
+          
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
+          <ThemedText type="link"></ThemedText>
         </ExternalLink>
       </Collapsible>
       <Collapsible title="Android, iOS, and web support">
@@ -41,15 +48,17 @@ export default function TabTwoScreen() {
           <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
         </ThemedText>
       </Collapsible>
-      <Collapsible title="Images">
+      <Collapsible title="jumpscare">
         <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
+           <ThemedText type="defaultSemiBold"></ThemedText> {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText> 
         </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
+        <Image
+        source={require('@/assets/images/gengar.webp')}
+        style={styles.headerImage}
+      />
+        <ExternalLink href="">
+          <ThemedText type="link"></ThemedText>
         </ExternalLink>
       </Collapsible>
       <Collapsible title="Custom fonts">
